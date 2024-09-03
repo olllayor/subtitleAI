a8d4038736753658d630201a456f230bd974a990
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-09-03 07:32:17 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ module.exports = {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:8000/:path*', // Adjust this to your backend URL
+          destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+        },
+        {
+          source: "/docs",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/docs"
+              : "/api/docs",
         },
       ]
     },
