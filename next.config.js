8929d91730a7e1b9566a9b089ec488aee63fe0b8
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-09-06 11:28:38 +0500

    set up check birth date

--- next.config.js
+++ next.config.js
@@ -11,21 +11,21 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "https://subtitle-ai-seven.vercel.app/api/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "https://subtitle-ai-seven.vercel.app/docs"
+            ? "http://127.0.0.1:8000/docs"
             : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "https://subtitle-ai-seven.vercel.app/openapi.json"
+            ? "http://127.0.0.1:8000/openapi.json"
             : "/api/openapi.json",
       },
     ];
