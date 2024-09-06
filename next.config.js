f5907c57df98234ad52935cdf32f31278e32903a
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-09-06 11:34:20 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8000/:path*"
             : "/api/",
       },
       {
