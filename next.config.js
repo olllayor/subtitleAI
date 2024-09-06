a80af62eee5b8926ebf8d36c3437fa3c35117c1e
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-09-06 11:03:09 +0500

    add tools for deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,35 @@
-module.exports = {
-    images: {
-        domains: ['media.giphy.com'], // Added the hostname to the images config
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: '/api/:path*',
-        },
-      ]
-    },
-  }
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['media.giphy.com'], // Added the hostname to the images config
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+      {
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/docs"
+            : "/api/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/openapi.json"
+            : "/api/openapi.json",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
